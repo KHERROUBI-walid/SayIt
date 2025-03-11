9e528be8c8b59c23118baaf400ff8a2979db5fc6
Kherroubi_Walid <kherroubi.walid78@gmail.com>
2025-03-11 11:20:47 +0100

    second commit

--- server/websocket.js
+++ server/websocket.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const axios = require('axios');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,7 +15,28 @@ io.on('connection', (socket) => {
   console.log('Un utilisateur s\'est connecté');
 
   socket.on('newPost', (post) => {
-    console.log('Nouveau post reçu :', post);
+    console.log('Nouveau post reçu:', post);
+
+    // Vérifier les données
+    if (!post.userName || !post.text) {
+      console.error('Données manquantes dans le post:', post);
+      return;
+    }
+
+    // Envoyer les données du post à l'API FastAPI
+    axios.post('http://localhost:8000/save_post', {
+      userName: post.userName,
+      text: post.text,
+      audioURL: post.audioURL || null 
+    })
+    .then(response => {
+      console.log('Réponse de FastAPI:', response.data);
+    })
+    .catch(error => {
+      console.error('Erreur lors de l\'envoi du post à FastAPI:', error.response ? error.response.data : error.message);
+    });
+
+    // Émettre l'événement 'postUpdate'
     io.emit('postUpdate', post);
   });
 
